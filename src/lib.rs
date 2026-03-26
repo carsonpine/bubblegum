48396d8b06e377f226b71d9b2b07b975f8483615
DavidNzube101 <carsonpine@hotmail.com>
2026-03-26 09:33:38 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub mod indexer;
 pub mod rpc;
 
 // Re-export commonly used types
-pub use decoder::{DecodedInstruction, DecodedAccount, BorshReader};
+pub use decoder::{DecodedInstruction, DecodedAccount};
 pub use idl::{Idl, ParsedIdl, compute_discriminator};
 pub use db::{PostgresDb, ClickhouseDb};
 pub use rpc::{HeliusRpcClient, RpcClientConfig};
